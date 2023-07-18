dd63ae272a3d0686be7a2297e7e8d2a0d43fca6e
Romjkensky <talaevroma@gmail.com>
2023-07-18 15:34:47 +0300

    Initial commit

---
+++ HomeWork4.8(2)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4._8_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2. Сложение матриц");
+            Console.WriteLine("Введите количество строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrixA = new int[row, col];
+            int[,] matrixB = new int[row, col];
+            int[,] matrixC = new int[row,col];
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Первая матрица: ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixA[i, j] = rnd.Next(10);
+                    Console.Write($"{matrixA[i, j],3}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Вторая матрица: ");
+            for(int i = 0;i < row; i++)
+            {
+                for(int j = 0;j < col; j++)
+                {
+                    matrixB[i, j] = rnd.Next(10);
+                    Console.Write($"{matrixB[i,j],3}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Сумма матриц: ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrixC[i,j] = matrixA[i, j] + matrixB[i,j];
+                    Console.Write($"{matrixC[i,j],3}");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ HomeWork4.8/Pract1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4._8_1
+{
+    internal class Pract1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1. Случайная матрица и сумма ее элементов");
+            Console.WriteLine("Введите количество строк: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов: ");
+            int col = int.Parse(Console.ReadLine());
+            
+            int[,] matrix = new int [row, col];
+            Random rnd = new Random();
+
+            Console.WriteLine("Ваша матрица: ");
+
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = rnd.Next(10);
+                    Console.Write($"{matrix[i, j],1} ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0;i < col; i++)
+            {
+                for(int j = 0;j < row; j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            Console.WriteLine($"Сумма всех элементов в матрице = {sum}");
+        }
+    }
+}
